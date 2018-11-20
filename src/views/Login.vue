2f68fdfce44848c4fb2f369d47c9b5d591d2daf1
hfq-jiaxinjie <jiaxinjie@huizhaofang.com>
2018-11-20 11:45:14 +0800

    pencil2 项目搭建-vue-cli3

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login-container"><h1>登录页面</h1></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.login-container
+  height 100%
+  width 100%
+</style>
